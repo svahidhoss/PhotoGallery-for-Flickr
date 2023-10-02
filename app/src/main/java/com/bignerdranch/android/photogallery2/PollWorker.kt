249da4f9d210b02d748f4e614096cf4759844235
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-10-02 13:50:53 -0700

    Ch22: Now starts the work by checking the latest query and result ids from Preference repo.

--- app/src/main/java/com/bignerdranch/android/photogallery2/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery2/PollWorker.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import kotlinx.coroutines.flow.first
 
 private const val TAG = "PollWorker"
 
@@ -16,7 +17,34 @@ class PollWorker(
 ) {
     override suspend fun doWork(): Result {
         Log.i(TAG, "Work request triggered")
-        return Result.success()
-    }
+        val preferencesRepository = PreferencesRepository.get()
+        val photoRepository = PhotoRepository()
 
+        val query = preferencesRepository.storedQuery.first()
+        val lastId = preferencesRepository.lastResultId.first()
+
+        if (query.isEmpty()) {
+            Log.i(TAG, "No saved query, finishing early")
+            return Result.success()
+        }
+
+        try {
+            val galleryItems = photoRepository.searchPhotos(query)
+
+            if (galleryItems.isNotEmpty()) {
+                val newResultId = galleryItems.first().id
+                if (newResultId == lastId) {
+                    Log.i(TAG, "Still have the same result: $newResultId")
+                } else {
+                    Log.i(TAG, "Got a new result: $newResultId")
+                    preferencesRepository.setLastResultId(newResultId)
+                }
+            }
+
+            return Result.success()
+        } catch (ex: Exception) {
+            Log.e(TAG, "")
+            return Result.failure()
+        }
+    }
 }
