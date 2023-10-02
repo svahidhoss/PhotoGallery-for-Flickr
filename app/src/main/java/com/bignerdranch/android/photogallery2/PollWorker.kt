40d77e1a9065a626e0372a726334f65d0aee8854
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-10-02 15:19:14 -0700

    Ch22: Adds notify user that shows a notification whenever new results have arrived from the API. Adds the required permission in AndroidManifest.xml file, and the required channel for the permission types in PhotoGalleryApplication.

--- app/src/main/java/com/bignerdranch/android/photogallery2/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery2/PollWorker.kt
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.photogallery2
 
+import android.app.PendingIntent
 import android.content.Context
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import kotlinx.coroutines.flow.first
@@ -38,6 +41,7 @@ class PollWorker(
                 } else {
                     Log.i(TAG, "Got a new result: $newResultId")
                     preferencesRepository.setLastResultId(newResultId)
+                    notifyUser()
                 }
             }
 
@@ -46,5 +50,28 @@ class PollWorker(
             Log.e(TAG, "")
             return Result.failure()
         }
+    }
+
+    private fun notifyUser() {
+        val intent = MainActivity.newIntent(context)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val resources = context.resources
+
+        val notification = NotificationCompat
+            .Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setTicker(resources.getString(R.string.new_pictures_title))
+            .setSmallIcon(android.R.drawable.ic_menu_report_image)
+            .setContentTitle(resources.getString(R.string.new_pictures_title))
+            .setContentText(resources.getString(R.string.new_pictures_text))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        NotificationManagerCompat.from(context).notify(0, notification)
     }
 }
