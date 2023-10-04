9a40ac3fae905088531434af80073474a2792898
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-10-04 15:50:54 -0700

    Ch23: Now loads the url into a webview

--- app/src/main/java/com/bignerdranch/android/photogallery2/PhotoPageFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery2/PhotoPageFragment.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.photogallery2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.photogallery2.databinding.FragmentPhotoPageBinding
 
 /**
@@ -12,12 +15,22 @@ import com.bignerdranch.android.photogallery2.databinding.FragmentPhotoPageBindi
  */
 class PhotoPageFragment : Fragment() {
 
+    private val args: PhotoPageFragmentArgs by navArgs()
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment using view binding
         val binding = FragmentPhotoPageBinding.inflate(inflater, container, false)
+        binding.apply {
+            webView.apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(args.photoPageUri.toString())
+            }
+        }
         return binding.root
     }
 }
